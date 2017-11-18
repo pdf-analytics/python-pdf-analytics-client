f499aca28e9de89395a7fe2bf7286d009fac0544
Evangelos Mantadakis <mantadakise@gmail.com>
2017-11-18 23:47:56 +0000

    Add more classifiers

--- setup.py
+++ setup.py
@@ -8,6 +8,34 @@ setup(
     license='MIT License ',
     author='PDF Analytics',
     author_email='info@pdf-analytics.com',
+    download_url='https://github.com/pdf-analytics/python-pdf-analytics-client',
+    keywords=['testing', 'pdf', 'pdf inspection', 'python', 'pdf inspection', 'automated testing',
+              'software testing', 'pdf-analytics.com'],
+
+    classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 5 - Production/Stable',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Testing',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+
+    'Operating System :: MacOS',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: Unix',
+],
     description='PyPDFAnalyticsClient allows you to automate most common PDFAnalytics (https://pdf-analytics.com) operations using Python 2. \n' +
                 'You may access the documentation page from here : http://pdf-analytics-client-library.readthedocs.io/en/latest/ \n' +
                 'You may access the GitHub repository from here : https://github.com/pdf-analytics/python-pdf-analytics-client .'
