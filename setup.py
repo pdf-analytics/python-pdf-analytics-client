e4e3ceac4f44d82dc59ff872cd94572fcb36bdfd
Evangelos Mantadakis <mantadakise@gmail.com>
2017-11-18 23:16:58 +0000

    Release 1.0.1 python-pdf-analytics-client

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from distutils.core import setup
 
 setup(
     name='python-pdf-analytics-client',
-    version='1.0.0',
+    version='1.0.1',
     packages=['pdf_analytics_client'],
     url='https://pdf-analytics.com',
     license='MIT License ',
     author='PDF Analytics',
     author_email='info@pdf-analytics.com',
-    description='PyPDFAnalyticsClient allows you to automate most common PDFAnalytics operations using Python 2.'
+    description='PyPDFAnalyticsClient allows you to automate most common PDFAnalytics (https://pdf-analytics.com) operations using Python 2. \n' +
+                'You may access the documentation page from here : http://pdf-analytics-client-library.readthedocs.io/en/latest/ \n' +
+                'You may access the GitHub repository from here : https://github.com/pdf-analytics/python-pdf-analytics-client .'
 )
