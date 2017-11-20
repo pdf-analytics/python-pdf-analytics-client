c56fc2056f27e19f47fdfcf9c92f562357186b38
Evangelos Mantadakis <mantadakise@gmail.com>
2017-11-20 22:17:32 +0000

    Add Changelog

---
+++ CHANGELOG.rst
@@ -0,0 +1,17 @@
+Changelog
+=========
+
+1.0.2
+-----
+#. Add Categories and keywords in pip / setup
+
+1.0.1
+-----
+#. Fix the SSL cert verification
+#. Fix the documentation
+#. Cosmetic changes to the python-behave examples
+
+1.0.0
+-----
+#. First release
+
--- setup.py
+++ setup.py
@@ -2,16 +2,14 @@ from distutils.core import setup
 
 setup(
     name='python-pdf-analytics-client',
-    version='1.0.1',
+    version='1.0.2',
     packages=['pdf_analytics_client'],
     url='https://pdf-analytics.com',
     license='MIT License ',
     author='PDF Analytics',
     author_email='info@pdf-analytics.com',
     download_url='https://github.com/pdf-analytics/python-pdf-analytics-client',
-    keywords=['testing', 'pdf', 'pdf inspection', 'python', 'pdf inspection', 'automated testing',
-              'software testing', 'pdf-analytics.com'],
-
+    keywords='testing pdf inspection python automated testing software testing pdf-analytics.com',
     classifiers=[
     # How mature is this project? Common values are
     #   3 - Alpha
@@ -36,7 +34,7 @@ setup(
     'Operating System :: Microsoft :: Windows',
     'Operating System :: Unix',
 ],
-    description='PyPDFAnalyticsClient allows you to automate most common PDFAnalytics (https://pdf-analytics.com) operations using Python 2. \n' +
-                'You may access the documentation page from here : http://pdf-analytics-client-library.readthedocs.io/en/latest/ \n' +
-                'You may access the GitHub repository from here : https://github.com/pdf-analytics/python-pdf-analytics-client .'
+    description='PyPDFAnalyticsClient allows you to automate most common PDFAnalytics (https://pdf-analytics.com) operations using Python 2. ',
+    long_description="PyPDFAnalyticsClient allows you to automate most common PDFAnalytics (https://pdf-analytics.com) operations using Python 2. You may access the documentation page from here : http://pdf-analytics-client-library.readthedocs.io/en/latest/. You may access the GitHub repository from here : https://github.com/pdf-analytics/python-pdf-analytics-client ."
+
 )
